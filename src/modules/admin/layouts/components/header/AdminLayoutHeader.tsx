c24c66ddfee10fd3a6e37f6165e8f40912ec51ba
tkuku <taras.kukuliak@aravo.com>
2026-04-23 15:56:17 +0300

    feat: Make header as link

--- src/modules/admin/layouts/components/header/AdminLayoutHeader.tsx
+++ src/modules/admin/layouts/components/header/AdminLayoutHeader.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import UserAvatar from "./components/user-avatar/UserAvatar.tsx";
 
 const AdminLayoutHeader = () => {
   return (
     <header className="bg-primary py-4 px-6 relative z-50">
       <div className="mx-auto w-full max-w-[1280px] flex items-center justify-between">
-        <div className="text-white text-2xl font-bold">Admin Header</div>
+        <Link to="/admin" className="text-white text-2xl font-bold">Admin</Link>
         <UserAvatar />
       </div>
     </header>
